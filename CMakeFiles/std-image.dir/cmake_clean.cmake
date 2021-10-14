572cca003acf25e7da4597ce95562833ef93f7ee
Twan Bolwerk <twanbolwerk@gmail.com>
2021-10-14 10:41:54 +0200

    added stb_image library

---
+++ CMakeFiles/std-image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/std-image.dir/main.c.o"
+  "std-image"
+  "std-image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/std-image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
