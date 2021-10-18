782b752985022bb741f02f3c292cdb2ed0a06e1e
Koen Bolhuis <koen.bolhuis@gmail.com>
2021-10-18 11:16:18 +0200

    Update radamsa.sh script to generate crashes indefinitely and collect results

--- radamsa.sh
+++ radamsa.sh
@@ -1,28 +1,71 @@
-# Run radamsa in a loop to generate fuzzed files until the stb-image wrapper program crashes;
-# The malformed file is then in $INPUT.fuzzed
+# radamsa.sh <infile> <seed>
+#
+# Seed should be a number, eg. `$RANDOM`.
+#
+# Run Radamsa in a loop to generate fuzzed files until the stb-image wrapper program crashes;
+# the malformed file is then collected under `./radamsa-output/`.
+# This is repeated forever.
 
 RADAMSA=../radamsa/bin/radamsa
 WRAPPER=build/stb-image
 
-# Input and output files
-if [ -z "$1" ]
-then
-    echo "Error: no input file given"
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+OUTDIR="radamsa-output/$TIMESTAMP"
+
+#################################################
+# Argument parsing
+#################################################
+
+if [ $# -lt 2 ]; then
+    echo "Usage: radamsa.sh <infile> <seed>"
     exit 1
 fi
-infile="$1"
-fuzzed="$infile.fuzzed"
 
-# Seed
-if [ ! -z "$2" ]
-then
-    RADAMSA="$RADAMSA --seed $2"
+# Input and output files
+inpath="$1"
+infile=$(basename $inpath)
+
+if [ ! -d "$OUTDIR" ]; then
+    mkdir -p "$OUTDIR"
+    echo "Created $OUTDIR"
 fi
 
-# Main loop
-while true
-do
-    $RADAMSA $infile > $fuzzed
-    $WRAPPER $fuzzed > /dev/null
-    test $? -gt 127 && break
-done
+output_file()
+{
+    echo "$OUTDIR/$seed_$1.$infile.fuzzed"
+}
+
+# Seed
+seed=$2
+
+#################################################
+# Main script
+#################################################
+
+# Run Radamsa until it crashes
+run_until_crash()
+{
+    local _sequential_seed="$seed$1"
+    local _outfile=$(output_file "$1")
+
+    while true; do
+        $RADAMSA --seed $_sequential_seed "$inpath" > "$_outfile"
+        $WRAPPER "$_outfile" > /dev/null
+        test $? -gt 127 && break
+    done
+
+    echo "$_outfile produced a crash"
+}
+
+# Main loop; do `run_until_crash` forever, collecting results in `$OUTDIR`
+main_loop()
+{
+    local _i=0
+    while true; do
+        echo "Iteration $_i: $(output_file $_i)"
+        run_until_crash $_i
+        i=$((i+1))
+    done
+}
+
+main_loop
