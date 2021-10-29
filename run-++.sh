eb563e9fbce27559bcd3c63435a71d5b6055ca88
Twan Bolwerk <twanbolwerk@gmail.com>
2021-10-29 09:50:50 +0200

    afl++ results

--- run-++.sh
+++ run-++.sh
@@ -1,4 +1,4 @@
 for i in {0..8}
 do
-	screen -dmS fuzzer$i /bin/bash -c "../AFLplusplus/afl-fuzz -i afl_testcases -o findings++ -M fuzzer$i -- build/stb-image @@"
+	screen -dmS fuzzer$i /bin/bash -c "../AFLplusplus/afl-fuzz -i afl_testcases/jpeg_turbo/edges-only -o findings++ -M fuzzer$i -- build/stb-image @@"
 done
