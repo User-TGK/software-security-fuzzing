4f4c7730b31dd0a05e7d3ea2d0ec68f7bd472474
Koen Bolhuis <koen.bolhuis@gmail.com>
2021-11-22 10:52:13 +0100

    Update radamsa2.sh

--- radamsa2.sh
+++ radamsa2.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-count=$(ls afl_testcases/jpeg_turbo/edges-only/images/ | wc -l)
+mkdir -p radamsa_output/fuzz/
+mkdir -p radamsa_output/crash/
+
+run_until_crash()
+{
+    while true; do
+        echo "Iteration: $1, mutations: $(($1 * $COUNT))"
+        ../radamsa/bin/radamsa -o radamsa_output/fuzz/%n-%s.fuzzed -n 1000 afl_testcases/jpeg_turbo/edges-only/images/*
+        FILES=radamsa_output/fuzz/*
+        for f in $FILES; do
+            output=$(build/stb-image $f 2>&1)
+            if [ $? -gt 127 ]; then
+                echo "$f caused a crash"
+                mkdir -p radamsa_output/crash/$1/
+                echo "$output" > "radamsa_output/crash/$1/crash.txt"
+                cp $f radamsa_output/crash/$1/
+            fi
+        done
+    done
+}
+
+COUNT=$(ls afl_testcases/jpeg_turbo/edges-only/images/ | wc -l)
 i=0
 while true; do
-    echo "Iteration: $i, mutations: $(($i * $count))"
-    ../radamsa/bin/radamsa -o radamsa_output/fuzz-%n-%s.fuzzed -n 1000 afl_testcases/jpeg_turbo/edges-only/images/*
-    FILES=radamsa_output/*
-    for f in $FILES; do
-        build/stb-image $f
-        if [ $? -gt 127 ]; then
-            echo "$f caused a crash"
-            exit 0
-        fi
-    done
+    run_until_crash $i
     i=$((i + 1))
 done
