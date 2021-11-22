6fb54ece398082bda302c7427ee94f29b790ac51
tpnbolwerk <twanbolwerk@gmail.com>
2021-11-22 09:46:25 +0100

    added fuzzer findings afl

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for i in {0..16}
+for i in {1..16}
 do
 	screen -dmS fuzzer$i /bin/bash -c "sudo afl-fuzz -i afl_testcases/png/edges-only/images/ -o findings -M fuzzer$i -- build/stb-image @@"
 done
